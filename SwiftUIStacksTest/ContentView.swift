6f6ce397de1aac51717e54cdcdd42b5dbbb4a85d
Hua Son Tung <tunghuason@gmail.com>
2021-01-09 14:09:13 +0700

    Initial Commit

---
+++ SwiftUIStacksTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStacksTest
+//
+//  Created by Hua Son Tung on 09/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIStacksTest/SwiftUIStacksTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksTestApp.swift
+//  SwiftUIStacksTest
+//
+//  Created by Hua Son Tung on 09/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
